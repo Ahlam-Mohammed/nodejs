0e3d575cb4d5581a47d5bb1004a43bfc27ad5344
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-02-15 02:10:52 +0300

    login route ,other route return 404

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
+app.get('/login', function(req, res) {
+    res.render('pages/login',{title:"login"});
+});
+
+const  credential = {
+    email : "admin@gmail.com",
+    password : "admin123"
+}
+
+// login user
+app.post('/log', (req, res)=>{
+    // if(req.body.email == credential.email && req.body.password == credential.password){
+    //     res.redirect('/');
+    // }else{
+    //     res.end("Invalid Username")
+    // }
+});
+
+// index page
 app.get('/', function(req, res) {
     res.render('pages/index',{title:"home"});
 });
@@ -31,6 +50,11 @@ app.get('/services', function(req, res) {
 // contact page
 app.get('/contact', function(req, res) {
     res.render('pages/contact',{title:"contact"});
+});
+
+// any age dose not exist
+app.use(function(req,res) {
+    res.status(404).send("page dose not exist")
 });
 
 app.listen(8080);
